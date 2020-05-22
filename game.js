98d612e21344dbd54c875b216d52dffa20961299
sjatin050 <63118687+sjatin050@users.noreply.github.com>
2020-05-22 18:39:53 +0530

    touch game

--- game.js
+++ game.js
@@ -14,6 +14,15 @@ $(document).keydown(function() {
   }
 });
 
+$(document).Touch(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
 $(".btn").click(function(){
   var userChosenColour=$(this).attr("id");
   userClickedPattern.push(userChosenColour);
